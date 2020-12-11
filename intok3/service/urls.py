d17be20d7557544d886b9b545ed61f722ec2daa7
mohsen <mohsenmarandy@gmail.com>
2020-12-11 12:00:02 +0330

    finish app service --- template +view(class base view)

--- intok3/service/urls.py
+++ intok3/service/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 
-from .views import categorylist,subcategorylist,servicelist
+from .views import categorylist,subcategorylist,servicelist,userlist
 urlpatterns = [
 
     path('',categorylist.as_view(),name='category-list'),
     path('show_subs/<int:cat_id>',subcategorylist.as_view(), name='subcategory-list'),
     path('show_services/<int:sub_id>',servicelist.as_view(), name='service-list'),
+    path('show_users/<int:service_id>',userlist.as_view(), name='user-list'),
+
 
 
 
--- intok3/service/views.py
+++ intok3/service/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from userprofile.models import *
 from .models import Subcategory, Category, Service
-from django.views.generic import ListView
-
+from django.views.generic import ListView,DetailView
+from django.views import View
 
 class categorylist(ListView):
     model = Category
@@ -22,5 +23,19 @@ class servicelist(ListView):
     def get_queryset(self):
 
         return Service.objects.filter(subcategory__id=self.kwargs['sub_id'])
+
+
+class userlist(View):
+    def get(self,request,service_id):
+        service_obj = get_object_or_404(Service, id=service_id)
+        username=service_obj.cuser
+        # users = service_obj.use.all()
+        user = cuser.objects.filter(username__contains=username)
+        # print(users)
+        # print(useraa)
+        context = {
+                'user':user
+                }
+        return render(request, 'user.html', context)
 
 
