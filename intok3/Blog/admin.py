2b65526ad49658a5d3200e035a08786468fee612
mohsen <mohsenmarandy@gmail.com>
2020-12-25 20:20:48 +0330

    add seo task

--- intok3/Blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Post , post_category
-
-admin.site.register(Post)
-admin.site.register(post_category)
--- intok3/cart/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from service.models import Service
-from userprofile.models import cuser
-
-class Cart(models.Model):
-    Service=models.ManyToManyField(Service)
-    cuser=models.ForeignKey(cuser,on_delete=models.CASCADE)
-    Quantity=models.IntegerField()
-    is_final=models.BooleanField(default=False)
